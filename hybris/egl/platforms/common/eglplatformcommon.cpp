1811ddb70dfeca54add6fcad1e03f5b033c26991
Deepak Meena <who53@disroot.org>
2026-01-05 07:45:46 +0530

    EGL_KHR_no_config_context
    Signed-off-by: Deepak Meena <who53@disroot.org>

--- hybris/egl/platforms/common/eglplatformcommon.cpp
+++ hybris/egl/platforms/common/eglplatformcommon.cpp
@@ -331,7 +331,7 @@ extern "C" const char *eglplatformcommon_eglQueryString(EGLDisplay dpy, EGLint n
 	{
 		const char *ret = (*real_eglQueryString)(dpy, name);
 		static char eglextensionsbuf[2048];
-		snprintf(eglextensionsbuf, 2046, "%sEGL_HYBRIS_native_buffer2 EGL_HYBRIS_WL_acquire_native_buffer EGL_WL_bind_wayland_display", ret ? ret : "");
+		snprintf(eglextensionsbuf, 2046, "%sEGL_HYBRIS_native_buffer2 EGL_KHR_no_config_context EGL_HYBRIS_WL_acquire_native_buffer EGL_WL_bind_wayland_display", ret ? ret : "");
 		ret = eglextensionsbuf;
 		return ret;
 	}
